4fd81f611d64ba44f58ed7594eeae12b2ed75825
Uday1004 <Udaysolanki530@gmail.com>
2023-09-26 11:31:49 +0530

    update

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,6 +4,7 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import vchat from "../../Assets/Projects/vchat.png";
 
+import hh from "../../Assets/Projects/hh.png";
 import tutils from "../../Assets/Projects/tutils.png";
 import mausamui from "../../Assets/Projects/mausamui.png";
 import portfolio from "../../Assets/Projects/portfolio.png";
@@ -45,23 +46,24 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={tutils}
+              imgPath={hh}
               isBlog={false}
-              title="Text-Utils"
-              description="Tutils, an innovative text manipulation website, revolutionizes the way we interact with and transform textual content. Boasting a sleek and user-friendly interface, Tutils empowers users to effortlessly manipulate and enhance text for a myriad of creative and practical purposes."
-              ghLink="https://github.com/Uday1004/Text-Utils"
-              demoLink="https://tutils.netlify.app"
+              title="HungryHubb | Online-Food ordering "
+              description= " HungryHubb is a cutting-edge online food website application that has revolutionized the way people discover, order, and enjoy their favorite meals. This innovative platform is designed to cater to the cravings of food enthusiasts by providing a seamless and convenient culinary experience."
+              ghLink="https://github.com/Uday1004/HungryHub"
+
+              demoLink="https://hungryhubb.netlify.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={vchat}
+              imgPath={tutils}
               isBlog={false}
-              title="V-Meet"
-              description="Introducing V-meet, an exceptional video chat clone that reimagines virtual communication, connecting individuals in a seamless and immersive way. With an intuitive interface and cutting-edge technology, V-meet offers an unparalleled video chat experience, bringing people together across distances."
-              ghLink="https://github.com/Uday1004/Video-Chat-Clon"
-              // demoLink="https://plant49-ai.herokuapp.com/"
+              title="Text-Utils"
+              description="TextUtils is an exceptional online text manipulation website that serves as a versatile toolkit for users seeking to perform a wide range of text-related tasks. This platform offers an array of powerful tools designed to streamline text processing, making it an indispensable resource for writers, developers, students, and professionals alike."
+              ghLink="https://github.com/Uday1004/Text-Utils"
+              demoLink="https://tutils.netlify.app/"
             />
           </Col>
 
@@ -71,7 +73,7 @@ function Projects() {
               isBlog={false}
               title="Portfolio"
               description="This project has been meticulously developed to provide a comprehensive insight into my portfolio, showcasing my professional journey, educational background, and other relevant accomplishments. It serves as a dedicated platform for you to gain an in-depth understanding of both my work and personal profile."
-              ghLink=""
+              ghLink="https://github.com/Uday1004/My-Portfolio"
               demoLink="https://udaysolanki.netlify.app/" /* <--------Please include a demo link here*/
             />
           </Col>
